bdaf032535b552bc46e237ab4badff9878f94706
Dumbeldor <vincent.glize@live.fr>
2015-03-02 20:37:07 +0100

    Fix error

--- experience/init.lua
+++ experience/init.lua
@@ -1,12 +1,12 @@
-homes_file = minetest.get_worldpath() .. "/experience"
+--homes_file = minetest.get_worldpath() .. "/experience"
 
 experience{}
 
 --Chargé les fonctions
-dofile(minetest.get_modpath("experience").."/function.lua")
+--dofile(minetest.get_modpath("experience").."/function.lua")
 
 loadExperience()
 
-dofile(minetest.get_modpath("experience").."/affichage.lua")
+--dofile(minetest.get_modpath("experience").."/affichage.lua")
 
-dofile(minetest.get_modpath("experience").."/interact.lua")
+--dofile(minetest.get_modpath("experience").."/interact.lua")
