ee3a40463a935a6f51b75efc1eb6275797020193
Dumbeldor <vincent.glize@live.fr>
2015-03-02 22:22:10 +0100

    Fix bug

--- economy/function.lua
+++ economy/function.lua
@@ -48,7 +48,7 @@ end
 
 function changeMess(pseudo)
 	local player = minetest.get_player_by_name(pseudo)
-	player:hud_change(idx, "Salut " .. player:get_player_name() .. "\n" .. "Portefeuille :".. argents[player:get_player_name()].argent .. nomMoney)
+	player:hud_change(idx, "text", "Salut " .. player:get_player_name() .. "\n" .. "Portefeuille :".. argents[player:get_player_name()].argent .. nomMoney)
 end
 
 local function loadEconomy()
